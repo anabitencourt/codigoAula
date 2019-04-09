abf1b0cad6c7ab447dd0f060540c2d15439af365
anabitencourt <48722988+anabitencourt@users.noreply.github.com>
2019-04-09 11:50:28 -0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+	<title>
+		Projeto App Minha Casa minha Vida
+	</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
+	</head>
+<body>
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+		<ul>
+		<li><a href="index1.php">HOME</a></li>
+		<li><a href="perfil.php">PERFIL</a></li>
+		<li><a href="contato.php">CONTATO</a></li>
+		</ul>
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Rafael A. Florindo<br>
+		<a href="mailto:rafael.florindo@escola.pr.gov.br">Contato</a>
+	</div>
+</body>
+</html>
