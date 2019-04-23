7f01d5931365c741bd9d238f881570cbe52174f4
anabitencourt <48722988+anabitencourt@users.noreply.github.com>
2019-04-23 11:53:14 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Prof. Rafael Alves Florindo</p>
+  <p>Curso Técnico em informática - Integrado</p>
+  <p>Turma: 3º. Informática</p>
+</div>
+
+</body>
+</html>
